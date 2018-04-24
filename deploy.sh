eb7b3d9c0292ddd5de724eebc9822c6e53a235c3
Joe Brady <joe.brady@uk.ibm.com>
2018-04-24 14:44:43 +0100

    Simplified Travis

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@
 echo --- Logging in to Cloud Foundry...
 "" | cf login -a $CF_API -u $CF_EMAIL -p $CF_PASS
 
+echo --- Getting target space name...
+APPS=`niet manifest.yml applications`
+for APP in "${APPS[@]}"; do
+    echo "${APP}"
+    # APPS=`niet APP`
+    # [[ $CF_APP_NAME == `niet APP.name` ]] && echo "$var present in the array"
+done
+
+echo --- Targeting $CF_SPACE...
+cf target -s $CF_SPACE
+
 echo --- Pushing $CF_APP_NAME to Cloud Foundry...
 cf push $CF_APP_NAME
 # cf blue-green-deploy id-generator-dev
